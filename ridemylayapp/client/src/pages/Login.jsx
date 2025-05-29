8955cb98122ab1d05c8f993708228baa6a06f29c
Hershel Thomas <hershel.thomas@post.runi.ac.il>
2025-05-29 14:12:08 +0300

    MAking screen size better on phones

--- ridemylayapp/client/src/pages/Login.jsx
+++ ridemylayapp/client/src/pages/Login.jsx
@@ -37,10 +37,9 @@ const Login = () => {
       navigate('/');
     }
   };
-  
-  return (
-    <div className="min-h-screen bg-gray-900 flex items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
-      <div className="max-w-md w-full space-y-8 bg-gray-800 p-8 rounded-xl shadow-lg">
+    return (
+    <div className="min-h-screen bg-gray-900 flex items-center justify-center py-4 px-4 sm:py-12 sm:px-6 lg:px-8">
+      <div className="w-full sm:max-w-md space-y-8 bg-gray-800 p-4 sm:p-8 rounded-xl shadow-lg">
         <div>
           <h2 className="mt-6 text-center text-3xl font-extrabold text-white">
             Sign in to your account
