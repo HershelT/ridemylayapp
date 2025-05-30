beb64bc26142f54d0278541f3479798bcbad65e5
Hershel Thomas <hershel.thomas@post.runi.ac.il>
2025-05-30 04:44:05 +0300

    Almost!

--- ridemylayapp/client/src/components/notifications/NotificationBadge.jsx
+++ ridemylayapp/client/src/components/notifications/NotificationBadge.jsx
@@ -39,6 +39,8 @@ const NotificationBadge = () => {
 
     return () => {
       clearInterval(refreshInterval);
+      window.removeEventListener('new_notification', handleNewNotification);
+      window.removeEventListener('socket_reconnected', handleReconnect);
     };
   }, [fetchUnreadCount]);
 
